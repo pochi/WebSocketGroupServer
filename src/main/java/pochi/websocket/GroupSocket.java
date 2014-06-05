0fa268de1944e9850b1d300235071d6cc6b85630
Yuichi Kuroda <pochi.black@gmail.com>
2014-06-06 07:21:34 +0900

    Avoid blocking message

--- src/main/java/pochi/websocket/GroupSocket.java
+++ src/main/java/pochi/websocket/GroupSocket.java
@@ -18,8 +18,8 @@ import javax.websocket.server.ServerEndpoint;
 @ServerEndpoint(value="/events/")
 public class GroupSocket
 {
-	public static HashMap<String, List<Session>> groups = new HashMap<String, List<Session>>();
-	
+		public static HashMap<String, List<Session>> groups = new HashMap<String, List<Session>>();
+
     @OnOpen
     public void onWebSocketConnect(Session session)
     {
@@ -28,11 +28,11 @@ public class GroupSocket
     	currentList = groups.containsKey(groupId) ? groups.get(groupId) : new ArrayList<Session>();
     	currentList.add(session);
     	groups.put(groupId, currentList);
-        
+
     	System.out.println("Socket Connected: " + session);
-        System.out.println("Group is: " + groupId + " , and currnet group size is : " + currentList.size());
+    	System.out.println("Group is: " + groupId + " , and currnet group size is : " + currentList.size());
     }
-    
+
     @OnMessage
     public void onWebSocketText(String message, Session session)
     {
@@ -40,26 +40,24 @@ public class GroupSocket
     	List<Session> currentList = groups.get(groupId);
     	for(Session currentSession : currentList) {
     		try {
-				currentSession.getBasicRemote().sendText(message);
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+					currentSession.getBasicRemote().sendStringByFuture(message);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
     	}
     }
-    
+
     @OnClose
     public void onWebSocketClose(CloseReason reason, Session session)
     {
     	String groupId = session.getRequestURI().getQuery();
     	List<Session> currentList = groups.get(groupId);
     	currentList.remove(session);
-    	
+
         System.out.println("Socket Closed: " + reason);
         System.out.println("Group is: " + groupId + " , and currnet group size is : " + currentList.size());
-
     }
-    
+
     @OnError
     public void onWebSocketError(Throwable cause)
     {
